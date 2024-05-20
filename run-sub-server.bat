dc6cf5aa89a18718c331a370c65ddec4795f9323
Tamir Hadash <udbe3333@gmail.com>
2024-05-20 20:50:16 +0300

    meow now you dont need to run dns stuff menually

--- run-sub-server.bat
+++ run-sub-server.bat
@@ -1,6 +1,8 @@
 @echo off
 
-python load_balancer_dnsclient.exe
+load_balancer_dnsclient.exe
+
+start /B cmd /c "dnssec_server.exe"
 
 REM Stop and remove existing containers and networks
 docker-compose -f docker-compose.yml down
